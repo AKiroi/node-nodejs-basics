24b8af2709e901e2fe3848c2b6ada18fdb4581b9
AKiroi <alexandr.kiroi@gmail.com>
2023-06-19 20:09:33 +0300

    feat: add solution to  rename

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,22 @@
-const remove = async () => {
-    // Write your code here 
+import { access, rm } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const pathDir = path.dirname(fileURLToPath(import.meta.url));
+
+const remove = async (pathFile) => {
+  try {
+    if (
+      !(await access(pathFile)
+        .then(() => true)
+        .catch(() => false))
+    ) {
+      throw new Error('fs operation failed');
+    }
+    await rm(pathFile);
+  } catch (e) {
+    console.error(e.message);
+  }
 };
 
-await remove();
+await remove(path.join(pathDir, 'files', 'fileToRemove.txt'));
