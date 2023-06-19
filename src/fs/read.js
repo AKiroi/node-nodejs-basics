dc30b42817b085808508b78a41f70bc7d46958c0
AKiroi <alexandr.kiroi@gmail.com>
2023-06-19 20:38:21 +0300

    feat: add solution to fs/read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
-const read = async () => {
-    // Write your code here 
+import { readFile, access } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const pathDir = path.dirname(fileURLToPath(import.meta.url));
+
+const read = async (pathFile) => {
+  try {
+    if (
+      !(await access(pathFile)
+        .then(() => true)
+        .catch(() => false))
+    ) {
+      throw new Error('FS operation failed');
+    }
+    console.log(await readFile(pathFile, { encoding: 'utf-8' }));
+  } catch (e) {
+    console.error(e.message);
+  }
 };
 
-await read();
+await read(path.join(pathDir, 'files', 'fileToRead.txt'));
