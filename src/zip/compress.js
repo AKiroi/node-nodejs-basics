62ece37512b33b28dce6e5c99c32b19fc8b438b2
AKiroi <alexandr.kiroi@gmail.com>
2023-06-19 23:17:44 +0300

    feat: add compress func to zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
-const compress = async () => {
-    // Write your code here 
+import { createReadStream, createWriteStream } from 'fs';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import { pipeline } from 'stream/promises';
+import { createGzip } from 'zlib';
+
+const pathDir = path.dirname(fileURLToPath(import.meta.url));
+
+const compress = async (curFilePath, arhFilePath) => {
+  try {
+    await pipeline(createReadStream(curFilePath), createGzip(), createWriteStream(arhFilePath));
+  } catch (e) {
+    console.error(e.message);
+  }
 };
 
-await compress();
+await compress(
+  path.join(pathDir, 'files', 'fileToCompress.txt'),
+  path.join(pathDir, 'files', 'archive.gz'),
+);
