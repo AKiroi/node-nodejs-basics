382df19b82c9e30409a1132cee5e43fba2e880e1
AKiroi <alexandr.kiroi@gmail.com>
2023-06-19 22:50:49 +0300

    feat: add transformStream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
+import { stdin, stdout } from 'process';
+
 const transform = async () => {
-    // Write your code here 
+    const transformStream  = new Transform({
+        transform(chunk, _, callback) {
+            callback(null, chunk.toString().split('').reverse().join(''));
+        }
+    })
+    await pipeline(stdin, transformStream, stdout);
 };
 
 await transform();
