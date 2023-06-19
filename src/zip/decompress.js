8d7d10e75bc8aed96d9c8eb4a49cb65d295d3140
AKiroi <alexandr.kiroi@gmail.com>
2023-06-19 23:20:57 +0300

    feat: add decompress to zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
-const decompress = async () => {
-    // Write your code here 
+import { createReadStream, createWriteStream } from 'fs';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import { pipeline } from 'stream/promises';
+import { createUnzip } from 'zlib';
+
+const pathDir = path.dirname(fileURLToPath(import.meta.url));
+
+const compress = async (arhFilePath, curFilePath) => {
+  try {
+    await pipeline(createReadStream(arhFilePath), createUnzip(), createWriteStream(curFilePath));
+  } catch (e) {
+    console.error(e.message);
+  }
 };
 
-await decompress();
+await compress(
+  path.join(pathDir, 'files', 'archive.gz'),
+  path.join(pathDir, 'files', 'fileToCompress.txt'),
+);
