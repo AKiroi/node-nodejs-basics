386df1ce7543dcc8b6818532c3181c23e3427aa4
AKiroi <alexandr.kiroi@gmail.com>
2023-06-20 00:12:01 +0300

    feat: add solution to Worker Threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,19 @@
-const performCalculations = async () => {
-    // Write your code here
+import { Worker } from 'worker_threads';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import os from 'os';
+
+const pathDir = path.dirname(fileURLToPath(import.meta.url));
+
+const performCalculations = async (pathWorkerFile) => {
+  const workers = Array.from({ length: os.cpus().length }, (_, i) => {
+    return new Promise((resolve) => {
+      const worker = new Worker(pathWorkerFile, { workerData: 10 + i });
+      worker.on('message', (data) => resolve({ status: 'resolved', data }));
+      worker.on('error', () => resolve({ status: 'error', data: null }));
+    });
+  });
+  console.log(await Promise.all(workers));
 };
 
-await performCalculations();
+await performCalculations(path.join(pathDir, 'worker.js'));
