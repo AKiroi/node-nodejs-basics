c0c528a31fae5d83bfbff290b92631bc64d13185
AKiroi <alexandr.kiroi@gmail.com>
2023-06-19 21:43:08 +0300

    feat: add parseArgs

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  const res = process.argv
+    .reduce((acc, item, i, arr) => {
+      if (item.startsWith('--')) {
+        acc.push(`${item.slice(2)} is ${arr[i + 1]}`);
+        return acc;
+      }
+      return acc;
+    }, [])
+    .join(', ');
+  console.log(res);
 };
 
 parseArgs();
