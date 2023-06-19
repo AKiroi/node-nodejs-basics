978e8c7417bf02b7312ad63de074dcdf3f4000f5
AKiroi <alexandr.kiroi@gmail.com>
2023-06-19 19:58:54 +0300

    feat: add solution to fs rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,26 @@
-const rename = async () => {
-    // Write your code here 
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const pathDir = path.dirname(fileURLToPath(import.meta.url));
+
+const rename = async (curFilePath, nextFilePath) => {
+  const isExists = async (path) =>
+    await fs.access(path)
+      .then(() => true)
+      .catch(() => false);
+
+  try {
+    if (!(await isExists(curFilePath)) || (await isExists(nextFilePath))) {
+      throw new Error('FS operation failed');
+    } 
+    await fs.rename(curFilePath, nextFilePath);
+  } catch (e) {
+    console.error(e.message);
+  }
 };
 
-await rename();
+await rename(
+  path.join(pathDir, 'files', 'wrongFilename.txt'),
+  path.join(pathDir, 'files', 'properFilename.md'),
+);
